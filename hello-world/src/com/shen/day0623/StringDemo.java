ffce52a3ba3f5440f0591974b59e4816774e2c3c
shen <yoxshen@163.com>
2018-07-04 21:42:20 +0800

    0714

---
+++ hello-world/src/com/shen/day0623/StringDemo.java
@@ -0,0 +1,11 @@
+package com.shen.day0623;
+
+import com.sun.org.apache.xpath.internal.operations.Equals;
+
+public class StringDemo {
+	public static void main(String[] args) {
+		String str = new String("abc");
+		String str1 = new String("abc");
+		System.out.println(str.equals(str1));
+	}
+}
---
+++ hello-world/src/com/shen/hello/OverLoadTest.java
@@ -0,0 +1,5 @@
+package com.shen.hello;
+
+public class OverLoadTest {
+
+}
